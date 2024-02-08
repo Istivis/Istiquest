5c7ed81ac3e1fc0a8ed6f218e244004176bc52c4
Thomas Styven <94020911+Istivis@users.noreply.github.com>
2024-02-08 10:52:31 -0300

    criação do projeto e commit

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Istiquest
+
+# ISTIQUEST
+
+Questionario criado com base na semana NLW da rocketseat.
+
+
+## Referência
+
+ - [Rocketseat](https://www.rocketseat.com.br/?utm_source=google&utm_medium=cpc&utm_campaign=lead&utm_term=perpetuo&utm_content=institucional-lead-home-texto-lead-brandkws-none-none-institucional-none-none-br-google)
+
+
+## Usado por
+
+Esse projeto é usado pelas seguintes pessoas:
+
+- Influencers
+- Uso Pessoal
+
+
+## Stack utilizada
+
+**Front-end:** Html, CSS, Javascript.
+
+## Imagem & Exemplo
+
+<img src="/imagem.png">
+
+
+## Documentação de cores
+
+| Cor               | Hexadecimal                                                |
+| ----------------- | ---------------------------------------------------------------- |
+| Cor exemplo       | ![#0f172a](https://via.placeholder.com/10/0f172a?text=+) #0f172a |
+| Cor exemplo       | ![#334155](https://via.placeholder.com/10/334155?text=+) #334155 |
+| Cor exemplo       | ![#f1f5f9](https://via.placeholder.com/10/f1f5f9?text=+) #f1f5f9 |
+| Cor exemplo       | ![#c52fc5](https://via.placeholder.com/10/c52fc5?text=+) #c52fc5 |
+| Cor exemplo       | ![#94a3b8](https://via.placeholder.com/10/94a3b8?text=+) #94a3b8 |
+| Cor exemplo       | ![#14c932](https://via.placeholder.com/10/14c93?text=+) #14c932 |
+| Cor exemplo       | ![#33ff00](https://via.placeholder.com/10/33ff00?text=+) #33ff00 |
+
+## Autores
+
+- [@Istivis](https://github.com/Istivis)
+
---
+++ index.js
@@ -0,0 +1,131 @@
+const perguntas = [
+    {
+      pergunta: "Qual é o nome do protagonista de Dragon Ball?",
+      respostas: [
+        "Son Goku",
+        "Vegeta",
+        "Piccolo",
+      ],
+      correta: 0 // Son Goku
+    },
+    {
+      pergunta: "Quem é o príncipe da raça Saiyajin?",
+      respostas: [
+        "Son Goku",
+        "Vegeta",
+        "Gohan",
+      ],
+      correta: 1 // Vegeta
+    },
+    {
+      pergunta: "Qual é a técnica de ataque característica de Piccolo?",
+      respostas: [
+        "Kamehameha",
+        "Final Flash",
+        "Special Beam Cannon",
+      ],
+      correta: 2 // Special Beam Cannon
+    },
+    {
+      pergunta: "Quem é o mestre de Son Goku?",
+      respostas: [
+        "Mestre Kame",
+        "Piccolo",
+        "Mr. Satan",
+      ],
+      correta: 0 // Mestre Kame
+    },
+    {
+      pergunta: "Qual é o nome da fusão de Goku e Vegeta?",
+      respostas: [
+        "Gogeta",
+        "Vegito",
+        "Gohan",
+      ],
+      correta: 1 // Vegito
+    },
+    {
+      pergunta: "Quem é o primeiro vilão enfrentado por Goku na série Dragon Ball Z?",
+      respostas: [
+        "Freeza",
+        "Cell",
+        "Raditz",
+      ],
+      correta: 2 // Raditz
+    },
+    {
+      pergunta: "Qual é o nome do planeta natal dos Saiyajins?",
+      respostas: [
+        "Terra",
+        "Namekusei",
+        "Vegeta",
+      ],
+      correta: 2 // Vegeta
+    },
+    {
+      pergunta: "Qual é o nome do irmão mais velho de Goku?",
+      respostas: [
+        "Gohan",
+        "Raditz",
+        "Goten",
+      ],
+      correta: 1 // Raditz
+    },
+    {
+      pergunta: "Quem é o príncipe dos Namekuseijins?",
+      respostas: [
+        "Dende",
+        "Piccolo",
+        "Nail",
+      ],
+      correta: 2 // Nail
+    },
+    {
+      pergunta: "Qual é o nome do pai de Goku?",
+      respostas: [
+        "Bardock",
+        "Rei Vegeta",
+        "Broly",
+      ],
+      correta: 0 // Bardock
+    },
+  ];
+
+  const quiz= document.querySelector('#quiz')
+  const template =document.querySelector('template')
+
+  const corretas= new Set()
+  const totalDePerguntas = perguntas.length
+  const mostrarTotal =document.querySelector('#acertos span')
+  mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+
+//loop 
+  for(const item of perguntas){
+    const quizItem = template.content.cloneNode(true)
+    quizItem.querySelector('h3').textContent =item.pergunta;
+
+      for(let resposta of item.respostas){
+        const dt= quizItem.querySelector ('dl dt').cloneNode(true)
+        dt.querySelector('span').textContent=resposta
+        dt.querySelector('input').setAttribute('name','pergunta-' + perguntas.indexOf(item))
+        dt.querySelector('input').value =item.respostas.indexOf(resposta)
+        dt.querySelector('input').onchange = (event) => {
+          const estaCorreta= event.target.value =item.correta
+
+          corretas.delete(item)
+          if(estaCorreta){
+            corretas.add(item)
+          }
+          mostrarTotal.textContent = corretas.size + ' de ' + totalDePerguntas
+        }
+
+
+
+
+        quizItem.querySelector('dl').appendChild(dt)
+      }
+//remover a pergunta template
+      quizItem.querySelector('dl dt').remove()
+      //coloca a pergunta na tela
+      quiz.appendChild(quizItem)
+  }
